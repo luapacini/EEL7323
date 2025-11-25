1617b30cfac548b874d1614ceea760cd24102a83
Luara <226411320+luapacini@users.noreply.github.com>
2025-11-25 15:37:49 -0300

    ajusta I2C para OO

--- projeto/I2C.cpp
+++ projeto/I2C.cpp
@@ -1,29 +1,9 @@
 #include "I2C.h"
 #include "Serial.h"
-
-#include <avr/io.h>
-#include <util/delay.h>
-
-#ifndef TWSR0
-    #undef TWBR0
-    #define TWBR0 TWBR
-    #define TWSR0 TWSR
-    #define TWAR0 TWAR
-    #define TWDR0 TWDR
-    #define TWCR0 TWCR
-    #define TWAMR0 TWAMR
-#endif
-
-
-#define BIT_SET(REG, BIT)        REG |=  (1<<BIT)
-#define BIT_CLEAR(REG, BIT)      REG &= ~(1<<BIT)
-
-#define BIT_CHECK(REG, BIT)      ((REG >> BIT) & 1)
-
-// ---------------------------------------------------------------------------------------------------
+---------------------------------------------------------------------------------------------------
 
 //seta os pinos de interrupcao e habilita comunicacao TWI 
-void I2C_Init(uint16_t speed)
+void I2C::Init(uint16_t speed)
 {
     TWSR0 = 0; //seta prescaler bits em 0
 
@@ -37,7 +17,7 @@ void I2C_Init(uint16_t speed)
 // ---------------------------------------------------------------------------------------------------
 
 //configura um timeout para a espera da flag TWINT ser setada
-bool I2C_TimeOut(void) 
+bool I2C::TimeOut(void) 
 {
     uint8_t delay = 200;
 
@@ -57,15 +37,15 @@ bool I2C_TimeOut(void)
 // ---------------------------------------------------------------------------------------------------
 
 //manda uma condicao de inicio
-void I2C_SendStart(void) 
+void I2C::SendStart(void) 
 {
     TWCR0 = (1 << TWINT) | (1 << TWSTA) | (1 << TWEN); 
-    I2C_TimeOut();
+    I2C::TimeOut();
 }
 // ---------------------------------------------------------------------------------------------------
 
 //manda uma condicao de parada
-void I2C_SendStop(void) 
+void I2C::SendStop(void) 
 {
     TWCR0 = (1 << TWINT) | (1 << TWEN) | (1 << TWSTO);
     uint8_t TOut = 100;
@@ -76,34 +56,34 @@ void I2C_SendStop(void)
 // ---------------------------------------------------------------------------------------------------
 
 //carrega o byte no registrador TWDR
-void I2C_WriteDataByte(uint8_t data) 
+void I2C::WriteDataByte(uint8_t data) 
 {
     TWDR0 = data; //seta o registrador TWDR com o dado a ser transmitido
     TWCR0 = (1 << TWINT) | (1 << TWEN); 
 
-    if (I2C_TimeOut()) {
+    if (I2C_::TimeOut()) {
         return;
     }
 }
 
 // ---------------------------------------------------------------------------------------------------
 
-void I2C_StartTransmission(uint8_t slave_address) 
+void I2C::StartTransmission(uint8_t slave_address) 
 {
-    I2C_SendStart();
-    I2C_WriteDataByte(slave_address << 1);
+    I2C::SendStart();
+    I2C::WriteDataByte(slave_address << 1);
 }
 
 // ---------------------------------------------------------------------------------------------------
 
-void I2C_RequestTransmission(uint8_t slave_address) 
+void I2C::RequestTransmission(uint8_t slave_address) 
 {
-    I2C_SendStart(); // envia a condicao de inicio
+    I2C::SendStart(); // envia a condicao de inicio
 
     TWDR0 = (slave_address << 1) | 0x01; //seta o registrador TWDR com o endereco do dispositivo e um bit de leitura
     TWCR0 = (1 << TWINT) | (1 << TWEN) | (1 << TWEA);
 
-    if (I2C_TimeOut()) {
+    if (I2C::TimeOut()) {
         return;
     }
 }
@@ -111,11 +91,11 @@ void I2C_RequestTransmission(uint8_t slave_address)
 // ---------------------------------------------------------------------------------------------------
 
 //le um byte de dado no registrador TWDR
-uint16_t I2C_ReadDataByte(bool ack) 
+uint16_t I2C::ReadDataByte(bool ack) 
 {
     TWCR0 = (1 << TWINT) | (1 << TWEN) | (ack ? 1 << TWEA : 0);
 
-    if (I2C_TimeOut()) {
+    if (I2C::TimeOut()) {
         return 0x8000;
     }
 
@@ -125,36 +105,36 @@ uint16_t I2C_ReadDataByte(bool ack)
 // ---------------------------------------------------------------------------------------------------
 
 //escreve um numero especificado de bytes de dado no registrador TWDR
-void I2C_WriteData(uint8_t device_address, uint8_t *data, uint8_t length)
+void I2C::WriteData(uint8_t device_address, uint8_t *data, uint8_t length)
 {
-    I2C_StartTransmission(device_address);
+    I2C::StartTransmission(device_address);
 
     for (int i = 0; i < length; i++) {
-        I2C_WriteDataByte(data[i]);
+        I2C::WriteDataByte(data[i]);
     }
 
-    I2C_SendStop();
+    I2C::SendStop();
 }
 
 // ---------------------------------------------------------------------------------------------------
 
 //le um numero especificado de bytes de dado no registrador TWDR
-void I2C_ReadData(uint8_t device_address, uint8_t *data, uint8_t bytes_num)
+void I2C::ReadData(uint8_t device_address, uint8_t *data, uint8_t bytes_num)
 {
     uint8_t index = 0;
 
-    I2C_RequestTransmission(device_address);
+    I2C::RequestTransmission(device_address);
 
     while (bytes_num > 0) {
         bytes_num--;
-        uint16_t byte_read = I2C_ReadDataByte((bytes_num > 0) ? true : false);
+        uint16_t byte_read = I2C::ReadDataByte((bytes_num > 0) ? true : false);
         if(byte_read == 0x8000){
             Serial::println("erro recebendo dados");
-            I2C_SendStop();
+            I2C::SendStop();
             return;
         }
         data[index++] = byte_read;
     }
     
-    I2C_SendStop();
+    I2C::SendStop();
 }
--- projeto/I2C.h
+++ projeto/I2C.h
@@ -2,11 +2,37 @@
 #define I2C_H
 
 #include <stdint.h>
+#include <avr/io.h>
+#include <util/delay.h>
 
-void I2C_Init(uint16_t speed);
-void I2C_StartTransmission(uint8_t slave_address);
-void I2C_RequestTransmission(uint8_t slave_address);
-void I2C_WriteData(uint8_t device_address, uint8_t *data, uint8_t length);
-void I2C_ReadData(uint8_t device_address,  uint8_t *data, uint8_t bytes_num);
+#ifndef TWSR0
+    #undef TWBR0
+    #define TWBR0 TWBR
+    #define TWSR0 TWSR
+    #define TWAR0 TWAR
+    #define TWDR0 TWDR
+    #define TWCR0 TWCR
+    #define TWAMR0 TWAMR
+#endif
+
+
+#define BIT_SET(REG, BIT)        REG |=  (1<<BIT)
+#define BIT_CLEAR(REG, BIT)      REG &= ~(1<<BIT)
+
+#define BIT_CHECK(REG, BIT)      ((REG >> BIT) & 1)
+
+class I2C {
+    private:
+        bool TimeOut(void); 
+        void SendStart(void);
+        void SendStop(void);
+        void WriteDataByte(uint8_t data);
+    public:
+        void Init(uint16_t speed);
+        void StartTransmission(uint8_t slave_address);
+        void RequestTransmission(uint8_t slave_address);
+        void WriteData(uint8_t device_address, uint8_t *data, uint8_t length);
+        void ReadData(uint8_t device_address,  uint8_t *data, uint8_t bytes_num);
+};
 
 #endif
