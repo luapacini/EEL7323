914a77d4bec1330eecb5f1372688817f1252afb6
Luara <226411320+luapacini@users.noreply.github.com>
2025-11-25 15:45:53 -0300

    ajusta SHT30

--- projeto/SHT30.cpp
+++ projeto/SHT30.cpp
@@ -1,67 +1,54 @@
-#include "SHT30.h"
-#include "Serial_0.h"
-#include "I2C.h"
 #include <util/delay.h>
 #include <stdio.h>
 
-uint8_t leitura[6];
-
-uint16_t rawTemperature, rawHumidity;
-uint8_t tempCRC, humCRC;
-uint16_t temp, umidade;
-uint8_t breakFlag = 0;
-
-uint8_t initCRC = 0xFF; //de acordo com datasheet
-uint8_t CRC_lookup[256] =  {0x00, 0x31, 0x62, 0x53, 0xC4, 0xF5, 0xA6, 0x97,   0xB9, 0x88, 0xDB, 0xEA, 0x7D, 0x4C, 0x1F, 0x2E,
-                            0x43, 0x72, 0x21, 0x10, 0x87, 0xB6, 0xE5, 0xD4,   0xFA, 0xCB, 0x98, 0xA9, 0x3E, 0x0F, 0x5C, 0x6D,
-                            0x86, 0xB7, 0xE4, 0xD5, 0x42, 0x73, 0x20, 0x11,   0x3F, 0x0E, 0x5D, 0x6C, 0xFB, 0xCA, 0x99, 0xA8,
-                            0xC5, 0xF4, 0xA7, 0x96, 0x01, 0x30, 0x63, 0x52,   0x7C, 0x4D, 0x1E, 0x2F, 0xB8, 0x89, 0xDA, 0xEB,
-                            0x3D, 0x0C, 0x5F, 0x6E, 0xF9, 0xC8, 0x9B, 0xAA,   0x84, 0xB5, 0xE6, 0xD7, 0x40, 0x71, 0x22, 0x13,
-                            0x7E, 0x4F, 0x1C, 0x2D, 0xBA, 0x8B, 0xD8, 0xE9,   0xC7, 0xF6, 0xA5, 0x94, 0x03, 0x32, 0x61, 0x50,
-                            0xBB, 0x8A, 0xD9, 0xE8, 0x7F, 0x4E, 0x1D, 0x2C,   0x02, 0x33, 0x60, 0x51, 0xC6, 0xF7, 0xA4, 0x95,
-                            0xF8, 0xC9, 0x9A, 0xAB, 0x3C, 0x0D, 0x5E, 0x6F,   0x41, 0x70, 0x23, 0x12, 0x85, 0xB4, 0xE7, 0xD6,
-                            0x7A, 0x4B, 0x18, 0x29, 0xBE, 0x8F, 0xDC, 0xED,   0xC3, 0xF2, 0xA1, 0x90, 0x07, 0x36, 0x65, 0x54,
-                            0x39, 0x08, 0x5B, 0x6A, 0xFD, 0xCC, 0x9F, 0xAE,   0x80, 0xB1, 0xE2, 0xD3, 0x44, 0x75, 0x26, 0x17,
-                            0xFC, 0xCD, 0x9E, 0xAF, 0x38, 0x09, 0x5A, 0x6B,   0x45, 0x74, 0x27, 0x16, 0x81, 0xB0, 0xE3, 0xD2,
-                            0xBF, 0x8E, 0xDD, 0xEC, 0x7B, 0x4A, 0x19, 0x28,   0x06, 0x37, 0x64, 0x55, 0xC2, 0xF3, 0xA0, 0x91,
-                            0x47, 0x76, 0x25, 0x14, 0x83, 0xB2, 0xE1, 0xD0,   0xFE, 0xCF, 0x9C, 0xAD, 0x3A, 0x0B, 0x58, 0x69,
-                            0x04, 0x35, 0x66, 0x57, 0xC0, 0xF1, 0xA2, 0x93,   0xBD, 0x8C, 0xDF, 0xEE, 0x79, 0x48, 0x1B, 0x2A,
-                            0xC1, 0xF0, 0xA3, 0x92, 0x05, 0x34, 0x67, 0x56,   0x78, 0x49, 0x1A, 0x2B, 0xBC, 0x8D, 0xDE, 0xEF,
-                            0x82, 0xB3, 0xE0, 0xD1, 0x46, 0x77, 0x24, 0x15,   0x3B, 0x0A, 0x59, 0x68, 0xFF, 0xCE, 0x9D, 0xAC};
-
-
-
-uint8_t crc8(uint8_t *msg, int lengthOfMsg, uint8_t init) {
-  while (lengthOfMsg-- > 0) init = CRC_lookup[ (init ^ *msg++)];
-  return init;
+#include "SHT30.h"
+#include "Serial.h"
+#include "I2C.h"
+    
+bool SHT30::getModoAquisicao(){
+    return modoAquisicao;
+}
+void SHT30::setModoAquisicao(bool modo){
+    modoAquisicao = modo;
 }
 
-void periodicAquisitionSetup() {
+uint8_t SHT30::crc8(uint8_t *msg, int lengthOfMsg, uint8_t init) {
+    while (lengthOfMsg-- > 0) init = CRC_lookup[ (init ^ *msg++)];
+    return init;
+}
+
+void SHT30::aquisitionSetup() {
     uint8_t msg[2];
+    
+    if(modoAquisicao) {
+        msg[0] = 0x21; //tabela 10 datasheet, 1mps, MSB
+        msg[1] = 0x30;//tabela 10 datasheet, high repeatability, LSB
+        Serial::println("--- Periodic Aquisition Mode ---"); Serial::println(" ");
+    } else {
+        msg[0] = 0x2C; //tabela 9 datasheet, MSB
+        msg[1] = 0x06; //tabela 9 datasheet, LSB
+        Serial::println("--- One Shot Aquisition Mode ---"); Serial::println("");
+    }
 
-    msg[0] = 0x21; //tabela 10 datasheet, 1mps, MSB
-    msg[1] = 0x30;//tabela 10 datasheet, high repeatability, LSB
-
-    I2C_WriteData(SHT30_ADDRESS, msg, 2); // Writes the data
-    Serial_0_println("--- Periodic Aquisition Mode ---"); Serial_0_println(" ");
+    I2C::WriteData(SHT30_ADDRESS, msg, 2); // Writes the data
 
     _delay_ms(3000);
 }
 
-uint8_t periodicAquisition() {
+uint8_t SHT30::periodicAquisition() {
     uint8_t msg[2];
 
     msg[0] = 0xE0; //tabela 11 datasheet /fetch data/
     msg[1] = 0x00; //tabela 11 datasheet /fetch data/
 
-    I2C_WriteData(SHT30_ADDRESS, msg, 2); // Writes the data
+    I2C::WriteData(SHT30_ADDRESS, msg, 2); // Writes the data
     
-    I2C_ReadData(SHT30_ADDRESS, leitura, 6);
+    I2C::ReadData(SHT30_ADDRESS, leitura, 6);
 
     if (breakFlag == 2) return 0; //nao imprimir duas vezes "Break conluido"
 
     if (breakFlag == 1) {
-        Serial_0_println("Break concluido");
+        Serial::println("Break concluido");
         breakFlag++;
         return 0;
     }
@@ -71,69 +58,56 @@ uint8_t periodicAquisition() {
     uint16_t rawHumidity = (uint16_t)((((uint16_t)leitura[3]) << 8) | leitura[4]); //mesma coisa umidade
     uint8_t humCRC = leitura[5];
 
-    if (crc8(&(leitura[0]), 2, initCRC) == tempCRC && crc8(&(leitura[3]), 2, initCRC) == humCRC) {
+    if (SHT30::crc8(&(leitura[0]), 2, initCRC) == tempCRC && SHT30::crc8(&(leitura[3]), 2, initCRC) == humCRC) {
         // Calculate temperature and humidity  
         temp = -45000 + 175000 * (rawTemperature / 65535.0); 
         umidade = 100 * (rawHumidity / 65535.0);
 
         char buffer[10];
         snprintf(buffer, 10, "%u", (unsigned int)(temp));
-        Serial_0_print("Temperatura = "); Serial_0_print(buffer); Serial_0_println(" mºC");
+        Serial::print("Temperatura = "); Serial::print(buffer); Serial::println(" mºC");
         snprintf(buffer, 10, "%u", (unsigned int) umidade);
-        Serial_0_print("Umidade = "); Serial_0_print(buffer); Serial_0_println("%"); Serial_0_println("");
+        Serial::print("Umidade = "); Serial::print(buffer); Serial::println("%"); Serial::println("");
         _delay_ms(2000);
     } else {
-        Serial_0_println("Houve problema no CRC, nao eh possivel printar resultado");
+        Serial::println("Houve problema no CRC, nao eh possivel printar resultado");
     }
     return 0;
 }
 
-uint8_t periodicAquisitionBreak() {
+uint8_t SHT30::periodicAquisitionBreak() {
     uint8_t msg[2];
 
     msg[0] = 0x30; 
     msg[1] = 0x93; 
 
-    I2C_WriteData(SHT30_ADDRESS, msg, 2); // Writes the data
+    I2C::WriteData(SHT30_ADDRESS, msg, 2); // escreve dados
 
-    Serial_0_println("--- Break ---"); Serial_0_println(" ");
+    Serial::println("--- Break ---"); Serial::println(" ");
     return 1;
 }
 
-void oneShotAquisitionSetup() {
-    uint8_t msg[2];
-
-    msg[0] = 0x2C; //tabela 9 datasheet, MSB
-    msg[1] = 0x06; //tabela 9 datasheet, LSB
-
-    I2C_WriteData(SHT30_ADDRESS, msg, 2); // Writes the data
-
-    Serial_0_println("--- One Shot Aquisition Mode ---"); Serial_0_println("");
-
-    _delay_ms(3000);
-}
-
-void oneShotAquisition() {
-    I2C_ReadData(SHT30_ADDRESS, leitura, 6);
+void SHT30::oneShotAquisition() {
+    I2C::ReadData(SHT30_ADDRESS, leitura, 6);
 
     uint16_t rawTemperature = (uint16_t)((((uint16_t)leitura[0]) << 8) | leitura[1]); // 8MSB da temperatura deslocados pra esquerda e ou logico com 8LSB
     uint8_t tempCRC = leitura[2];
     uint16_t rawHumidity = (uint16_t)((((uint16_t)leitura[3]) << 8) | leitura[4]); //mesma coisa umidade
     uint8_t humCRC = leitura[5];
 
-    if (crc8(&(leitura[0]), 2, initCRC) == tempCRC && crc8(&(leitura[3]), 2, initCRC) == humCRC) {
+    if (SHT30::crc8(&(leitura[0]), 2, initCRC) == tempCRC && SHT30::crc8(&(leitura[3]), 2, initCRC) == humCRC) {
         // Calculate temperature and humidity  
         temp = -45000 + 175000 * (rawTemperature / 65535.0); 
         umidade = 100 * (rawHumidity / 65535.0);
 
         char buffer[10];
         snprintf(buffer, 10, "%u", (unsigned int)(temp));
-        Serial_0_print("Temperatura = "); Serial_0_print(buffer); Serial_0_println(" mºC");
+        Serial::print("Temperatura = "); Serial::print(buffer); Serial::println(" mºC");
         snprintf(buffer, 10, "%u", (unsigned int) umidade);
 
-        Serial_0_print("Umidade = "); Serial_0_print(buffer); Serial_0_println("%"); Serial_0_println("");
+        Serial::print("Umidade = "); Serial::print(buffer); Serial::println("%"); Serial::println("");
         _delay_ms(2000);
     } else {
-        Serial_0_println("Houve problema no CRC, nao eh possivel printar resultado");
+        Serial::println("Houve problema no CRC, nao eh possivel printar resultado");
     }
 }
