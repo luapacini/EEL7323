cda2292916038130b492643aaf140e97a8422aa0
Luara <226411320+luapacini@users.noreply.github.com>
2025-12-03 15:08:40 -0300

    pequenos ajustes na identacao

--- projeto/RegistroCSV.h
+++ projeto/RegistroCSV.h
@@ -9,36 +9,33 @@
 #define MAX_REGISTROS 50
 
 class RegistroCSV {
-public:
-    struct POD {
+    private:
         float temperatura;
         float umidade;
         float luminosidade;
+
+    public:
+        RegistroCSV();
+        struct POD {
+            float temperatura;
+            float umidade;
+            float luminosidade;
+        };
+
+        void setTemp(float t);
+        void setUmid(float u);
+        void setLumin(float l);
+
+        float getTemp() const;
+        float getUmid() const;
+        float getLumin() const;
+
+        POD toPOD() const;
+        void fromPOD(const POD& pod);
     };
-
-private:
-    float temperatura;
-    float umidade;
-    float luminosidade;
-
-public:
-    RegistroCSV();
-
-    void setTemp(float t);
-    void setUmid(float u);
-    void setLumin(float l);
-
-    float getTemp() const;
-    float getUmid() const;
-    float getLumin() const;
-
-    POD toPOD() const;
-    void fromPOD(const POD& pod);
-};
 
 // Funções para salvar/ler EEPROM
 void initEEPROM_CSV();
 void salvarCSVexcecao(class SHT30 &sht, class LDR &ldr);
-RegistroCSV lerRegistro(uint16_t index);
 
 #endif
